8003474d94c34ada818d7882677f23697a7378ec
vaishanvi Tomar <vaishanvi-tomar@RT0217245>
2025-08-11 16:09:08 +0530

    Add SPA Redirect config for render

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 import { EmailGeneratorComponent } from './components/email-generator/email-generator.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'generate', pathMatch: 'full' },
     { path: 'generate', component: EmailGeneratorComponent },
+     { path: '', redirectTo: 'generate', pathMatch: 'full' },
     { path: '**', redirectTo: 'generate' }
+
 ];
