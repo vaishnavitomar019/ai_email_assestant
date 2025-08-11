363d87dc01525d0069dfeb3d7050e36d34c951db
vaishanvi Tomar <vaishanvi-tomar@RT0217245>
2025-08-11 15:38:41 +0530

    env production url change2

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: false,
   // apiBaseUrl: 'http://localhost:5000/api'
-    apiBaseUrl: 'https://ai-email-assestant.onrender.com/'
+    apiBaseUrl: 'https://ai-email-assestant.onrender.com/api'
 
 };
