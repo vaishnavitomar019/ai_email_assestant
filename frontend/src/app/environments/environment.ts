2d71bfcd9927c950ee2c7c07868fc68ab313cca9
vaishanvi Tomar <vaishanvi-tomar@RT0217245>
2025-08-12 10:48:43 +0530

    change the api base url of production to local

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  // apiBaseUrl: 'http://localhost:5000/api'
-    apiBaseUrl: 'https://ai-email-assestant.onrender.com/api'
+   apiBaseUrl: 'http://localhost:5000/api'
+    // apiBaseUrl: 'https://ai-email-assestant.onrender.com/api'
 
 };
